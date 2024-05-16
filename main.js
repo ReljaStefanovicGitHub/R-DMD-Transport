e1f9a86dd9b5062dd28bad200cda9b5a8a33616f
Your Name <you@example.com>
2024-05-16 22:01:27 +0200

    favicons added

--- main.js
+++ main.js
@@ -21,3 +21,93 @@ function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
+
+
+
+
+
+
+
+
+
+
+
+myButton.addEventListener(
+    "click",
+    function () {
+        myPopup.classList.add("show");
+    }
+);
+closePopup.addEventListener(
+    "click",
+    function () {
+        myPopup.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == myPopup) {
+            myPopup.classList.remove(
+                "show"
+            );
+        }
+    }
+);
+
+
+
+
+
+myButton1.addEventListener(
+    "click",
+    function () {
+        myPopup1.classList.add("show");
+    }
+);
+closePopup1.addEventListener(
+    "click",
+    function () {
+        myPopup1.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == myPopup1) {
+            myPopup1.classList.remove(
+                "show"
+            );
+        }
+    }
+);
+
+
+myButton2.addEventListener(
+    "click",
+    function () {
+        myPopup2.classList.add("show");
+    }
+);
+closePopup2.addEventListener(
+    "click",
+    function () {
+        myPopup2.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == myPopup2) {
+            myPopup2.classList.remove(
+                "show"
+            );
+        }
+    }
+);
